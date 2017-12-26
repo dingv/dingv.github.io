da5d3b0dbb18a8b90385800f3fa81e073fec45e0
Valerie Ding <12373919+dingv@users.noreply.github.com>
2017-12-26 13:04:26 -0800

    Add files via upload

---
+++ infini-treat/generator.js
@@ -0,0 +1,26 @@
+var basket = {};
+
+function generate() {
+	var treats = ["Butterfinger", 
+	"KitKat", 
+	"Mr. Goodbar",
+	"Crunch Bar", 
+	"Raisins"];
+	
+	var rindex = Math.floor(Math.random() * treats.length);
+	treat = treats[rindex];
+	if (basket.hasOwnProperty(treat)) {
+		basket[treat] += 1;
+	} else {
+		basket[treat] = 1;
+	}
+	document.getElementById("treat").innerHTML = treats[rindex];
+	
+	baskettxt = "";
+	for (var k in basket) {
+		if (basket.hasOwnProperty(k)) {
+			baskettxt = baskettxt + k + ": " + basket[k] + "<br>";
+		}
+	}
+	document.getElementById("basket").innerHTML = "<b>Your basket:</b><br>" + baskettxt;
+}
