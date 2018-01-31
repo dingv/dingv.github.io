7c52ca22a688d6cfbe5aff851a0ca770ddda73f3
Valerie Ding <valerie.ding@gmail.com>
2018-01-31 04:08:24 -0800

    Add files via upload

--- stream/script.js
+++ stream/script.js
@@ -1,9 +1,11 @@
 function pressEnter() {
 	var input = document.getElementById("message").value;
-	document.getElementById("output").innerHTML += input + "<br>";
-	document.getElementById("message").value = "";
-}
 
-function clearf() {
-	document.getElementById("output").innerHTML = "";
+	if (input == ":clear") {
+		document.getElementById("output").innerHTML = "";
+	} else {
+		document.getElementById("output").innerHTML += input + "<br>";
+	}
+
+	document.getElementById("message").value = "";
 }
