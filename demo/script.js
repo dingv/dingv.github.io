ad115df88652d0b57baf9a2ab32c5a225922c4a1
Valerie Ding <valerie.ding@gmail.com>
2018-02-02 15:26:23 -0800

    reorg

--- demo/script.js
+++ demo/script.js
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Demo</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<style>
-canvas {
-    border:1px solid #d3d3d3;
-    background-color: #f1f1f1;
-}
-</style>
-</head>
-
-<body onload="startGame()">
-
-<p id="log"></p>
-
-<script>
 var myGamePiece;
 var npc1;
 
@@ -93,8 +75,3 @@ function updateGameArea() {
     // collision
     if (myGamePiece.x > npc1.x) { document.getElementById("log").innerHTML = "hi"; }
 }
-</script>
-
-
-</body>
-</html>
